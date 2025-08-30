0622abee6db834a0bb06f60f64bc2c20d38544a5
habibur5313 <habiburmamun313@gmail.com>
2025-08-30 03:40:41 -0600

    part-6

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,23 @@
 import App from "@/App";
 import DashboardLayout from "@/components/layout/DashboardLayout";
+import About from "@/pages/About";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import Verify from "@/pages/Verify";
 import { generateRoutes } from "@/utils/generateRoutes";
 import { createBrowserRouter, Navigate } from "react-router";
 import { adminSidebarItems } from "./adminSidebarItems";
+import { userSidebarItems } from "./userSidebarItems";
+import { withAuth } from "@/utils/withAuth";
 import Unauthorized from "@/pages/Unauthorized";
 import { role } from "@/constants/role";
-import type { TRole } from "@/types";
-import { withAuth } from "@/utils/withAuth";
-import { About } from "@/pages/About";
-import { userSidebarItems } from "./userSidebarItems";
+import { type TRole } from "@/types";
+import Tours from "@/pages/Tours";
+import TourDetails from "@/pages/TourDetails";
+import Booking from "@/pages/Booking";
+import Success from "@/pages/Payment/Success";
+import Fail from "@/pages/Payment/Fail";
+import Homepage from "@/pages/HomePage";
 
 export const router = createBrowserRouter([
   {
@@ -19,8 +25,24 @@ export const router = createBrowserRouter([
     path: "/",
     children: [
       {
-        Component: withAuth(About),
+        Component: Homepage,
+        index: true,
+      },
+      {
+        Component: About,
         path: "about",
+      },
+      {
+        Component: Tours,
+        path: "tours",
+      },
+      {
+        Component: TourDetails,
+        path: "tours/:id",
+      },
+      {
+        Component: withAuth(Booking),
+        path: "booking/:id",
       },
     ],
   },
@@ -55,5 +77,13 @@ export const router = createBrowserRouter([
   {
     Component: Unauthorized,
     path: "/unauthorized",
+  },
+  {
+    Component: Success,
+    path: "/payment/success",
+  },
+  {
+    Component: Fail,
+    path: "/payment/fail",
   },
 ]);
